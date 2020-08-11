0d47f863f6d00f4b0e0d92c9bf32c0fefa2d7d70
Minecodes <65566012+Minecodes@users.noreply.github.com>
2020-08-11 18:40:09 +0200

    Create scan.sh

---
+++ scan.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+mkdir /home/pap/loot/nmap
+nmap -sn 192.168.0.0/24 -oN `/home/pap/loot/nmap/00-network`
+nmap -sn 192.168.1.0/24 -oN `/home/pap/loot/nmap/01-network`
+nmap -sn 192.168.2.0/24 -oN `/home/pap/loot/nmap/02-network`
+nmap -sn 192.168.3.0/24 -oN `/home/pap/loot/nmap/03-network`
+nmap -sn 192.168.4.0/24 -oN `/home/pap/loot/nmap/04-network`
+nmap -sn 192.168.5.0/24 -oN `/home/pap/loot/nmap/05-network`
+nmap -sn 192.168.6.0/24 -oN `/home/pap/loot/nmap/06-network`
+nmap -sn 192.168.7.0/24 -oN `/home/pap/loot/nmap/07-network`
+nmap -sn 192.168.8.0/24 -oN `/home/pap/loot/nmap/08-network`
+nmap -sn 192.168.9.0/24 -oN `/home/pap/loot/nmap/09-network`
+nmap -sn 192.168.10.0/24 -oN `/home/pap/loot/nmap/10-network`
+nmap -sn 192.168.11.0/24 -oN `/home/pap/loot/nmap/11-network`
+nmap -sn 192.168.12.0/24 -oN `/home/pap/loot/nmap/12-network`
+nmap -sn 192.168.13.0/24 -oN `/home/pap/loot/nmap/13-network`
+nmap -sn 192.168.14.0/24 -oN `/home/pap/loot/nmap/14-network`
+nmap -sn 192.168.15.0/24 -oN `/home/pap/loot/nmap/15-network`
+nmap -sn 192.168.16.0/24 -oN `/home/pap/loot/nmap/16-network`
+nmap -sn 192.168.17.0/24 -oN `/home/pap/loot/nmap/17-network`
+nmap -sn 192.168.18.0/24 -oN `/home/pap/loot/nmap/18-network`
+nmap -sn 192.168.19.0/24 -oN `/home/pap/loot/nmap/19-network`
+nmap -sn 192.168.20.0/24 -oN `/home/pap/loot/nmap/20-network`
+nmap -sn 192.168.21.0/24 -oN `/home/pap/loot/nmap/21-network`
+nmap -sn 192.168.22.0/24 -oN `/home/pap/loot/nmap/22-network`
+nmap -sn 192.168.23.0/24 -oN `/home/pap/loot/nmap/23-network`
+nmap -sn 192.168.24.0/24 -oN `/home/pap/loot/nmap/24-network`
+nmap -sn 192.168.25.0/24 -oN `/home/pap/loot/nmap/25-network`
+nmap -sn 192.168.26.0/24 -oN `/home/pap/loot/nmap/26-network`
+nmap -sn 192.168.27.0/24 -oN `/home/pap/loot/nmap/27-network`
+nmap -sn 192.168.28.0/24 -oN `/home/pap/loot/nmap/28-network`
+nmap -sn 192.168.29.0/24 -oN `/home/pap/loot/nmap/29-network`
+nmap -sn 192.168.30.0/24 -oN `/home/pap/loot/nmap/30-network`
